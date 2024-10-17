5f266438b91db829011b98007fd958165b9e606c
eugeniojosemouraneto <eugeniojosemouraneto092@gmail.com>
2024-10-17 14:44:52 -0300

    [front] criação do layout de duas columas na pagina de menu de tarefas vigentes

--- front-agenda/src/App.tsx
+++ front-agenda/src/App.tsx
@@ -1,4 +1,5 @@
 import './global.css'
+import style from './App.module.css'
 
 // components
 import Header from './components/Header/Header'
@@ -7,41 +8,33 @@ import { TaskSummary } from './components/task/taskSummary'
 export default function App() {
   return (
     <div>
-      <Header 
-          titlePage = "Igan, menu de tarefas"
-      />
-      <TaskSummary 
-          title='AED1 ENG1550 Eletrônica Geral'
-          categoty='Eletrônica Geral'
-          dateMaximumComplet='14/10/2024'
-          anexo={null}
-          porcentCompleted={58}
-          description='Descrição:  Projetar uma fonte de alimentação conforme figura abaixo para atender uma determinada carga RL, grupo Máximo de 3 alunos, entregar DIA 14-10-2024, entregar fonte montada funcionando e relatório de cálculo e custo da fonte:'
-      />
-      <TaskSummary 
-          title='AED1 ENG1550 Eletrônica Geral'
-          categoty='Eletrônica Geral'
-          dateMaximumComplet='14/10/2024'
-          anexo={null}
-          porcentCompleted={58}
-          description='Descrição:  Projetar uma fonte de alimentação conforme figura abaixo para atender uma determinada carga RL, grupo Máximo de 3 alunos, entregar DIA 14-10-2024, entregar fonte montada funcionando e relatório de cálculo e custo da fonte:'
-      />
-      <TaskSummary 
-          title='AED1 ENG1550 Eletrônica Geral'
-          categoty='Eletrônica Geral'
-          dateMaximumComplet='14/10/2024'
-          anexo={null}
-          porcentCompleted={58}
-          description='Descrição:  Projetar uma fonte de alimentação conforme figura abaixo para atender uma determinada carga RL, grupo Máximo de 3 alunos, entregar DIA 14-10-2024, entregar fonte montada funcionando e relatório de cálculo e custo da fonte:'
-      />
-      <TaskSummary 
-          title='AED1 ENG1550 Eletrônica Geral'
-          categoty='Eletrônica Geral'
-          dateMaximumComplet='14/10/2024'
-          anexo={null}
-          porcentCompleted={58}
-          description='Descrição:  Projetar uma fonte de alimentação conforme figura abaixo para atender uma determinada carga RL, grupo Máximo de 3 alunos, entregar DIA 14-10-2024, entregar fonte montada funcionando e relatório de cálculo e custo da fonte:'
-      />
+        <Header 
+            titlePage = "Igan, menu de tarefas"
+        />
+        <div className={style.content}>
+            <div className={style.column01}>
+                <h2>Minhas categorias</h2>
+            </div>
+            <div>
+                <h2>Minhas tarefas</h2>
+                <TaskSummary 
+                    title='AED1 ENG1550 Eletrônica Geral'
+                    categoty='Eletrônica Geral'
+                    dateMaximumComplet='14/10/2024'
+                    anexo={null}
+                    porcentCompleted={58}
+                    description='Descrição:  Projetar uma fonte de alimentação conforme figura abaixo para atender uma determinada carga RL, grupo Máximo de 3 alunos, entregar DIA 14-10-2024, entregar fonte montada funcionando e relatório de cálculo e custo da fonte:'
+                />
+                <TaskSummary 
+                    title='AED1 ENG1550 Eletrônica Geral'
+                    categoty='Eletrônica Geral'
+                    dateMaximumComplet='14/10/2024'
+                    anexo={null}
+                    porcentCompleted={58}
+                    description='Descrição:  Projetar uma fonte de alimentação conforme figura abaixo para atender uma determinada carga RL, grupo Máximo de 3 alunos, entregar DIA 14-10-2024, entregar fonte montada funcionando e relatório de cálculo e custo da fonte:'
+                />
+            </div>
+        </div>
     </div>
  )
 }
